329f865cceb6d877e4b2a44eed4ba1e4a8504cee
GuilhermeFiani <gfiani10@gmail.com>
2025-06-29 02:38:12 -0400

    feat: adiciona tela do anfitrião

--- hospedagens/app/src/main/java/com/example/hospedagens/utils/PasswordUtils.java
+++ hospedagens/app/src/main/java/com/example/hospedagens/utils/PasswordUtils.java
@@ -1,4 +1,35 @@
 package com.example.hospedagens.utils;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+// Classe utilitária que faz o hash das senhas
 public class PasswordUtils {
+
+    // Hash com MessageDigest (achei mais simples que com o BCrypt)
+    public static String hashPassword(String password) {
+        // Trata exceção para o caso de o algoritmo SHA-256 não estar implementado no ambiente Java
+        try {
+            // Gera valor de hash de 256 bits
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes());
+            StringBuilder hexString = new StringBuilder();
+
+            // Converte os bytes obtidos para sua versão hexadecimal de 2 dígitos (32 bytes)
+            for (byte b : hash) {
+                String hex = Integer.toHexString(0xff & b);
+                if (hex.length() == 1) {
+                    hexString.append('0');
+                }
+                hexString.append(hex);
+            }
+
+            // Retorna para o armazenamento no Banco
+            return hexString.toString();
+        } catch (NoSuchAlgorithmException e) {
+            // Se encontrar a exceção, vai exibir na saída o caminho da pilha que teve problema (essa aqui), para possível depuração.
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
